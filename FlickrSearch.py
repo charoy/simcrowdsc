4ceac15b6297961d37eb21898b4618348a13235a
François Charoy <francois.charoy@loria.fr>
2015-07-01 17:03:45 +0200

    start from day 1

--- FlickrSearch.py
+++ FlickrSearch.py
@@ -7,17 +7,17 @@ __author__ = 'charoy'
 user = flickr_api.Person.findByUserName("francois54")
 print(user)
 area="-74.5354,40.2275,-73.2567,40.9926"
-starttime=1351468800 # oct 10 2012
+starttime=1351468800+86400 # oct 10 2012
 duration=86400 # one day
 
-for d in range(7):
+for d in range(6):
     p= flickr_api.Photo.search(bbox=area,min_upload_date=str(starttime),max_upload_date=str(starttime+duration),page=1)
-    print ("Day "+str(d))
+    print ("Day "+str(d+1))
     print(p)
     print p.info.pages # the number of available pages of results
     print p.info.page  # the current page number
     print p.info.total # total number of photos
-    result=open(str(d)+"positions.csv","w")
+    result=open(str(d+1)+"positions.csv","w")
     for j in range(p.info.pages):
         for i in p:
             try:
@@ -25,10 +25,10 @@ for d in range(7):
                 loc=flickr_api.Photo.getLocation(i)
                 print(loc)
                 result.write(str(loc.latitude)+";"+str(loc.longitude)+"\n")
-                i.save("images/"+"day"+str(d)+"-"+str(i.id)+".jpg",size_label = 'Medium 640')
+                i.save("images/"+"day"+str(d+1)+"-"+str(i.id)+".jpg",size_label = 'Medium 640')
             except:
                 print "Unexpected error"
                 pass
         p= flickr_api.Photo.search(bbox=area,min_upload_date=str(starttime),max_upload_date=str(starttime+duration),page=j)
         print(p)
-    f.close()
+    result.close()
