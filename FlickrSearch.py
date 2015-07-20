d3729b277baaf416e4607632e2fbe6ac723b8216
François Charoy <francois.charoy@loria.fr>
2015-07-20 19:58:00 +0200

    tacloban

--- FlickrSearch.py
+++ FlickrSearch.py
@@ -15,7 +15,7 @@ print(user)
 #starttime=1298332800 # Tue, 22 Feb 2011 00:00:00 GMT
 
 #tacloban
-area="124.904938,11.180687,125.030594,11.352143"
+area="124.738829,11.115,125.139069,11.467063"
 starttime=1383696000 #  Wed, 06 Nov 2013 00:00:00 GMT
 
 duration=86400 # one day
